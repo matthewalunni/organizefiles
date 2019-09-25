3cf71373e2062d6eb070d02a8db6ca2a9facb3a6
ajdeziel <matthew.alunni@gmail.com>
2019-09-25 11:37:32 -0400

    Added mappings.

---
+++ organizefiles.py
@@ -0,0 +1,13 @@
+
+
+# the directory to be watched
+directory = "C:\\Users\\matth\\Documents"
+
+# these are the mappings for the program
+# if a file contains the key, then it is moved to the corresponding value
+mappings = {
+    "4F00": "C:\\Users\\matth\\Documents\\School\\COSC 4F00",
+    "3P97": "C:\\Users\\matth\\Documents\\School\\COSC 3P97",
+    "2P95": "C:\\Users\\matth\\Documents\\School\\COSC 2P95"
+}
+
