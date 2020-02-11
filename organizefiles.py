df5ef61ad7875cbb775ace652b389d7deda0613a
matthewalunni <matthew.alunni@gmail.com>
2020-02-10 19:06:09 -0500

    Allow for multiple configurations

--- organizefiles.py
+++ organizefiles.py
@@ -1,36 +1,60 @@
 import os
 import time
+import json
 import shutil
-from watchdog.events import FileSystemEventHandler, PatternMatchingEventHandler
-from watchdog.observers import Observer 
 
-# the directory to be watched
-directory = "C:\\Users\\matth\\Documents"
+configurations = [
+    {
+    "directory": "C:\\Users\\matth\\Documents",
+    "mappings": {
+        "4F00": "C:\\Users\\matth\\Documents\\School\\COSC 4F00\\",
+        "3P97": "C:\\Users\\matth\\Documents\\School\\COSC 3P97\\",
+        "2P95": "C:\\Users\\matth\\Documents\\School\\COSC 2P95\\",
+        "4P14": "C:\\Users\\matth\\Documents\\School\\COSC 4P14\\",
+        "3P91": "C:\\Users\\matth\\Documents\\School\\COSC 3P91\\",
+        "ENSU 2P02": "C:\\Users\\matth\\Documents\\School\\ENSU 2P02\\",
+        "MHD": "C:\\Users\\matth\\Documents\\Projects\\Web Development\\Maidens Hockey Development\\",
+        "VET": "C:\\Users\\matth\\Documents\\Projects\\Web Development\\Visual-Edge-Technology\\",
+        "86thestigma": "C:\\Users\\matth\\Documents\\Projects\\Web Development\\86thestigma\\",
+    }
+    },
+    {
+    "directory": "C:\\Users\\matth\\Downloads",
+    "mappings": {
+        "4F00": "C:\\Users\\matth\\Documents\\School\\COSC 4F00\\",
+        "3P97": "C:\\Users\\matth\\Documents\\School\\COSC 3P97\\",
+        "2P95": "C:\\Users\\matth\\Documents\\School\\COSC 2P95\\",
+        "4P14": "C:\\Users\\matth\\Documents\\School\\COSC 4P14\\",
+        "3P91": "C:\\Users\\matth\\Documents\\School\\COSC 3P91\\",
+        "ENSU 2P02": "C:\\Users\\matth\\Documents\\School\\ENSU 2P02\\",
+        "MHD": "C:\\Users\\matth\\Documents\\Projects\\Web Development\\Maidens Hockey Development\\",
+        "VET": "C:\\Users\\matth\\Documents\\Projects\\Web Development\\Visual-Edge-Technology\\",
+        "86thestigma": "C:\\Users\\matth\\Documents\\Projects\\Web Development\\86thestigma\\",
+    }
+    }
+]
 
-# these are the mappings for the program
-# if a file contains the key, then it is moved to the corresponding value
-mappings = {
-    "4F00": "C:\\Users\\matth\\Documents\\School\\COSC 4F00\\",
-    "3P97": "C:\\Users\\matth\\Documents\\School\\COSC 3P97\\",
-    "2P95": "C:\\Users\\matth\\Documents\\School\\COSC 2P95\\"
-}
-
-# this class handles what happens when a file is modified in a directory
-class OrganizationHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        for filename in os.listdir(directory):
-            if (stringContainsKey(mappings, filename)):
-                _key = getKey(mappings, filename)
-                toDestination = mappings[_key]
-                fromFile = directory + "\\" + filename
-                try:
-                    shutil.move(fromFile, toDestination)
-                    pass
-                except Exception:
-                    pass
-                
-
+           
+def main():
+    for config in configurations: 
+        dir = config["directory"]
+        mappings = config["mappings"]
+        print(str("Searching: " + dir))
+        for filename in os.listdir(dir):
+                # for each filename in the directory
+                # if the filename contains the mapping key
+                if (stringContainsKey(mappings, filename)):
+                    _key = getKey(mappings, filename)
+                    toDestination = mappings[_key]
+                    fromFile = dir + "\\" + filename
+                    try:
+                        shutil.move(fromFile, toDestination)
+                        print("Moved File: " + fromFile + os.linesep + "To: " + toDestination)
+                        pass
+                    except Exception:
+                        pass
                         
+
 # this method checks if a string contains a key of a dictionary
 # @param dictionary     dictionary to be checked
 # @param _str           string to be compared with keys            
@@ -52,14 +76,6 @@ def getKey(dictionary, string):
     
     return ""
 
-event_handler = OrganizationHandler()
-observer = Observer()
-observer.schedule(event_handler, directory, recursive=True)
-observer.start()
 
-try:
-    while True:           
-        time.sleep(10)
-except KeyboardInterrupt:
-    observer.stop()
-observer.join()
+if __name__ == "__main__":
+    main()
